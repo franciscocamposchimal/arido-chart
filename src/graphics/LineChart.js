442e5faf3c93465daaf645d3a5a974afbdd8760d
Francisco Campos <camposfrancisci@gmail.com>
2020-05-07 19:17:09 -0500

    feat(chartjs): view single charet

--- src/graphics/LineChart.js
+++ src/graphics/LineChart.js
@@ -1,14 +1,31 @@
 /* eslint-disable */
 import { Line, mixins } from 'vue-chartjs';
 const { reactiveProp } = mixins;
-
+let canvasWidth = 0;
+let canvasHeight = 0;
 export default {
 	extends: Line,
 	mixins: [ reactiveProp ],
-	props: [ 'options' ],
+	props: [ 'chartData', 'options' ],
+	watch: {
+		options: {
+			handler(newOption, oldOption) {
+				this.$data._chart.destroy();
+				this.renderChart(this.chartData, newOption);
+
+				if (this.$refs.canvas.width > 0) {
+					canvasWidth = this.$refs.canvas.width;
+				}
+				if (this.$refs.canvas.height > 0) {
+					canvasHeight = this.$refs.canvas.height;
+				}
+				this.$refs.canvas.style.cssText = `display: block; width: ${canvasWidth}px; height: ${canvasHeight}px;`;
+				// console.log(this.$refs.canvas.style.cssText);
+			},
+			deep: true
+		}
+	},
 	mounted() {
-		// this.chartData is created in the mixin.
-		// If you want to pass options please create a local options object
 		this.renderChart(this.chartData, this.options);
 	}
 };
--- src/views/Single.vue
+++ src/views/Single.vue
@@ -14,13 +14,16 @@
               <v-col cols="12">
                 <v-card class="pa-2" outlined tile elevation="4">
                   <v-card-text>
-                    <v-row no-gutters>
-                      <v-col cols="3">
-                        {{ itemToGraph.status.title }}
-                        <v-icon :color="itemToGraph.status.color">{{ itemToGraph.status.icon }}</v-icon>
+                    <v-row no-gutters justify="space-between" align="center">
+                      <v-col cols="2">
+                        {{ itemsToGraph[tab].status.title }}
+                        <v-icon
+                          :color="itemsToGraph[tab].status.color"
+                        >{{ itemsToGraph[tab].status.icon }}</v-icon>
                       </v-col>
-                      <v-col cols="3">
+                      <v-col cols="2">
                         <v-select
+                          v-model="itemsToGraph[tab]"
                           :items="item"
                           label="Sensors"
                           dense
@@ -31,10 +34,45 @@
                           @change="itemSelected"
                         ></v-select>
                       </v-col>
+                      <v-col cols="2">
+                        <v-select
+                          v-model="unitToGraph[tab]"
+                          :items="unitsToSelect[tab]"
+                          label="Units"
+                          dense
+                          outlined
+                          item-text="name"
+                          item-value="tag"
+                          return-object
+                          @change="unitSelected"
+                        ></v-select>
+                      </v-col>
+                      <v-col cols="2">
+                        <v-select
+                          v-model="unitsTimeToGraph[tab]"
+                          :items="unitsTimeToSelect"
+                          label="Time"
+                          dense
+                          outlined
+                          item-text="name"
+                          item-value="tag"
+                          return-object
+                          @change="timeSelected"
+                        ></v-select>
+                      </v-col>
                     </v-row>
                   </v-card-text>
+
+                  <line-chart
+                    :ref="`tabSelectted`"
+                    :chart-data="itemsToGraph[tab].data"
+                    :options="optionsChart"
+                  ></line-chart>
+
+                  <pre>{{ $vuetify.breakpoint.name }}</pre>
+                  <pre>{{ $vuetify.breakpoint.width }} - {{ $vuetify.breakpoint.height }}</pre>
                   <v-card-actions>
-                    <v-btn text>Descargar</v-btn>
+                    <v-btn text>Download</v-btn>
                   </v-card-actions>
                 </v-card>
               </v-col>
@@ -46,58 +84,116 @@
   </v-container>
 </template>
 <script>
+import LineChart from "@/graphics/LineChart.js";
+// :width="300" :height="300"
 export default {
   name: "Single",
+  components: {
+    LineChart
+  },
   data() {
     return {
-      tab: null,
-      items: ["Temperature", "Pressure"],
-      itemToGraph: {
-        status: {
-          title: "Offline",
-          icon: "mdi-flash-alert",
-          color: "red"
-        }
+      tab: 0,
+      unit: "",
+      time: "",
+      styleChart: {
+        width: "100%",
+        height: "100%"
       },
-      tabsSelect: [
+      items: ["Pressure", "Temperature"],
+      itemsToGraph: [
+        {
+          id: 0,
+          name: "",
+          status: {
+            title: "Online",
+            icon: "mdi-flash",
+            color: "green"
+          },
+          data: this.fillData(1, 5000000)
+        },
+        {
+          id: 0,
+          name: "",
+          status: {
+            title: "Online",
+            icon: "mdi-flash",
+            color: "green"
+          },
+          data: this.fillData(1, 1000000)
+        }
+      ],
+      unitsTimeToGraph: [
+        {
+          tag: "min",
+          name: "(min) Minutes"
+        },
+        {
+          tag: "min",
+          name: "(min) Minutes"
+        }
+      ],
+      unitsTimeToSelect: [
+        {
+          tag: "min",
+          name: "(min) Minutes"
+        },
+        {
+          tag: "seg",
+          name: "(s) Seconds"
+        },
+        {
+          tag: "hr",
+          name: "(hr) Hours"
+        }
+      ],
+      unitToGraph: [
+        {
+          tag: "PSI",
+          name: "(PSI) Pounds per square inch"
+        },
+        {
+          tag: "ºC",
+          name: "(ºC) Celsius"
+        }
+      ],
+      unitsToSelect: [
         [
           {
-            id: 1,
-            name: "Sensor T1",
-            status: {
-              title: "Offline",
-              icon: "mdi-flash-alert",
-              color: "red"
-            }
+            tag: "PSI",
+            name: "(PSI) Pounds per square inch"
           },
           {
-            id: 2,
-            name: "Sensor T2",
-            status: {
-              title: "Online",
-              icon: "mdi-flash",
-              color: "green"
-            }
+            tag: "PA",
+            name: "(PA) Pascal"
           },
           {
-            id: 3,
-            name: "Sensor T3",
-            status: {
-              title: "Offline",
-              icon: "mdi-flash-alert",
-              color: "red"
-            }
+            tag: "MPa",
+            name: "(MPa) Megapascal"
           }
         ],
+        [
+          {
+            tag: "ºC",
+            name: "(ºC) Celsius"
+          },
+          {
+            tag: "ºF",
+            name: "(ºF) Fahrenheit"
+          }
+        ]
+      ],
+      tabsSelect: [
         [
           {
             id: 1,
             name: "Sensor P1",
             status: {
-              title: "Offline",
-              icon: "mdi-flash-alert",
-              color: "red"
-            }
+              title: "Online",
+              icon: "mdi-flash",
+              color: "green"
+            },
+            data: this.fillData(1, 5000000)
           },
           {
             id: 2,
@@ -106,7 +202,8 @@ export default {
               title: "Online",
               icon: "mdi-flash",
               color: "green"
-            }
+            },
+            data: this.fillData(1, 5000000)
           },
           {
             id: 3,
@@ -115,27 +212,151 @@ export default {
               title: "Offline",
               icon: "mdi-flash-alert",
               color: "red"
-            }
+            },
+            data: this.fillData(1, 5000000)
+          }
+        ],
+        [
+          {
+            id: 1,
+            name: "Sensor T1",
+            status: {
+              title: "Offline",
+              icon: "mdi-flash-alert",
+              color: "red"
+            },
+            data: this.fillData(1, 5000000)
+          },
+          {
+            id: 2,
+            name: "Sensor T2",
+            status: {
+              title: "Online",
+              icon: "mdi-flash",
+              color: "green"
+            },
+            data: this.fillData(1, 5000000)
+          },
+          {
+            id: 3,
+            name: "Sensor T3",
+            status: {
+              title: "Offline",
+              icon: "mdi-flash-alert",
+              color: "red"
+            },
+            data: this.fillData(1, 5000000)
           }
         ]
       ]
     };
   },
+  computed: {
+    optionsChart() {
+      return {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: `Time (${this.time})`
+              },
+              ticks: {
+                min: 0,
+                max: 30,
+                stepSize: 1
+              }
+            }
+          ],
+          yAxes: [
+            {
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: this.unit
+              },
+              ticks: {
+                min: 100,
+                max: 500,
+                stepSize: 100
+              }
+            }
+          ]
+        }
+      };
+    }
+  },
   methods: {
     itemSelected({ status }) {
       // console.log(status);
-      this.itemToGraph.status = { ...status };
+      this.itemsToGraph[this.tab].status = { ...status };
+    },
+    unitSelected(unit) {
+      this.unit = unit.tag;
+      // console.log(this.optionsChart);
+    },
+    timeSelected(time) {
+      this.time = time.tag;
+      // console.log(this.optionsChart);
+    },
+    fillData(hours, colorMax) {
+      const hexColor = this.getRandomHexColor(colorMax);
+      return {
+        labels: this.timeToLabel(hours),
+        datasets: [
+          {
+            label: "data uno",
+            backgroundColor: hexColor,
+            borderColor: hexColor,
+            fill: false,
+            data: this.getRandomInt(hours, 100, 500)
+          }
+        ]
+      };
+    },
+    getRandomInt(hours, min, max) {
+      const toMinutes = Math.floor(hours * 60);
+      const timeList = Array.from(Array(toMinutes), () =>
+        Math.floor(Math.random() * (max - min) + min)
+      );
+      // console.log(timeList);
+      return timeList;
+    },
+    timeToLabel(hours) {
+      const toMinutes = Math.floor(hours * 60);
+      // const toSeconds = Math.floor(hours * 60 * 60);
+      const minutesList = Array.from(Array(toMinutes), (x, index) => index + 1);
+      // console.log(minutesList);
+      return minutesList;
+    },
+    getRandomHexColor(colorMax) {
+      // 1000000
+      let n = (Math.random() * 0xfffff * colorMax).toString(16);
+      return `#${n.slice(0, 6)}`;
     }
   },
   watch: {
-    tab() {
-      // console.log(tabSelect);
-      this.itemToGraph.status = {
-        title: "Offline",
-        icon: "mdi-flash-alert",
-        color: "red"
-      };
+    tab(tabSelect) {
+      console.log("tabSelect");
+      // const forTab = this.tabsSelect[tabSelect];
+      // this.itemsToGraph[tabSelect] = forTab[tabSelect];
+      this.unit = this.unitToGraph[tabSelect].tag;
+      this.time = this.unitsTimeToGraph[tabSelect].tag;
+      // console.log("tabSelect ", this.itemsToGraph);
+      // console.log();
     }
+  },
+  beforeMount() {
+    const forTabOne = this.tabsSelect[0];
+    const forTabTwo = this.tabsSelect[1];
+    this.itemsToGraph[0] = forTabOne[0];
+    this.itemsToGraph[1] = forTabTwo[0];
+    this.unit = this.unitToGraph[this.tab].tag;
+    this.time = this.unitsTimeToGraph[this.tab].tag;
+    // console.log('beforeMount', this.itemsToGraph);
   }
 };
 </script>
