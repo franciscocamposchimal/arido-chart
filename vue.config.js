684e3262595a0aeaa7a212bec4a27b198856c6f1
Francisco Campos <camposfrancisci@gmail.com>
2020-06-23 23:11:10 -0500

    fix(vue-config): wss

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
-    // https: true,
+    hotOnly: true,
     disableHostCheck: true,
-    // host: 'localhost'
-    // public: 'https://argos.arido.dev'
+    clientLogLevel: 'warning',
+    inline: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    },
   }
 };
