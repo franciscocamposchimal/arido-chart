5c38ffe64de6d406740172969870fc32a6b82c66
Francisco Campos <camposfrancisci@gmail.com>
2020-06-23 23:15:20 -0500

    fix(vue-config): wss

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
+    port: '8080',
+    host: '0.0.0.0',
     hotOnly: true,
     disableHostCheck: true,
     clientLogLevel: 'warning',
